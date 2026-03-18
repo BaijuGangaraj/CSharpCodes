94b941e38f5bf08165acc9f3cb55027df8874256
Baiju G <46807768+BaijuGangaraj@users.noreply.github.com>
2026-03-19 00:36:50 +0530

    Add string reverse program in C#
    Added a C# example that reverses a string manually without using built-in reverse methods.
    This demonstrates basic looping, indexing, and string manipulation logic.
    Example input: "I am Developer"
    Output: "repoleveD ma I"

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        string str = "Iam a Developer";
+        string result = "";
+
+        for (int i=str.Length-1;i>=0;i--)
+        {
+            result = result + str[i];
+        }
+
+        Console.WriteLine(result);
+    }
+}
